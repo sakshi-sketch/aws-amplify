cbe475be0750f83f8ef7509f42762e259831d351
Sakshigarbhe <112876251+sakshi-sketch@users.noreply.github.com>
2024-09-24 10:01:51 +0530

    feat: changed message

---
+++ src/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './page.css'
+
+const page = () => {
+  return (
+    <div className='card'>
+        <h3>Sakshi Garbhe</h3>
+        <h2>Div : BE 5</h2>
+        <h2>Roll No:42321</h2>
+    </div>
+  )
+}
+
+export default page
